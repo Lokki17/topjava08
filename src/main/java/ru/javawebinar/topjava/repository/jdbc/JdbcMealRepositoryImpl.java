8ae4fda602df932254007113b40ca990770c9ceb
lokki17 <df1985@mail.ru>
2016-10-11 10:14:01 +0300

    HW05

--- src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcMealRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcMealRepositoryImpl.java
@@ -42,28 +42,7 @@ public abstract class JdbcMealRepositoryImpl implements MealRepository {
     }
 
     @Override
-    public abstract Meal save(Meal meal, int userId);/* {
-        MapSqlParameterSource map = new MapSqlParameterSource()
-                .addValue("id", meal.getId())
-                .addValue("description", meal.getDescription())
-                .addValue("calories", meal.getCalories())
-                .addValue("date_time", meal.getDateTime())
-                .addValue("user_id", userId);
-
-        if (meal.isNew()) {
-            Number newId = insertMeal.executeAndReturnKey(map);
-            meal.setId(newId.intValue());
-        } else {
-            if (namedParameterJdbcTemplate.update("" +
-                            "UPDATE meals " +
-                            "   SET description=:description, calories=:calories, date_time=:date_time " +
-                            " WHERE id=:id AND user_id=:user_id"
-                    , map) == 0) {
-                return null;
-            }
-        }
-        return meal;
-    }*/
+    public abstract Meal save(Meal meal, int userId);
 
     @Override
     public boolean delete(int id, int userId) {
@@ -84,9 +63,6 @@ public abstract class JdbcMealRepositoryImpl implements MealRepository {
     }
 
     @Override
-    public abstract List<Meal> getBetween(LocalDateTime startDate, LocalDateTime endDate, int userId);/* {
-        return jdbcTemplate.query(
-                "SELECT * FROM meals WHERE user_id=?  AND date_time BETWEEN  ? AND ? ORDER BY date_time DESC",
-                ROW_MAPPER, userId, startDate, endDate);
-    }*/
+    public abstract List<Meal> getBetween(LocalDateTime startDate, LocalDateTime endDate, int userId);
+
 }
