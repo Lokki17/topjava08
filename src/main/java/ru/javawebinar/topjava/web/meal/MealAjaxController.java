7f6de08221378e8bf6688bdef2ff000edcb7db03
lokki17 <df1985@mail.ru>
2016-10-30 12:06:06 +0300

    HW8

--- src/main/java/ru/javawebinar/topjava/web/meal/MealAjaxController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealAjaxController.java
@@ -10,7 +10,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
-@RequestMapping("ajax/meals")
+@RequestMapping("/ajax/meals")
 public class MealAjaxController extends AbstractMealController{
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
@@ -28,11 +28,13 @@ public class MealAjaxController extends AbstractMealController{
         super.delete(id);
     }
 
-    @PutMapping(value = "/{id}")
+    @PostMapping()
     public void createOrUpdate(@RequestParam("id") int id,
                                @RequestParam("dateTime") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime dateTime,
                                @RequestParam("description") String description,
-                               @RequestParam("calories") int calories) {
+                               @RequestParam("calories") String calorie) {
+        System.out.println(calorie);
+        int calories = Integer.valueOf(calorie);
         Meal meal = new Meal(id, dateTime, description, calories);
         if (meal.isNew()){
             super.create(meal);
@@ -41,10 +43,10 @@ public class MealAjaxController extends AbstractMealController{
         }
     }
 
-    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
-    public Meal create(Meal meal) {
+/*    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Meal create(@RequestBody Meal meal) {
         return super.create(meal);
-    }
+    }*/
 
     @GetMapping(value = "/between", produces = MediaType.APPLICATION_JSON_VALUE)
     public List<MealWithExceed> getBetween(
