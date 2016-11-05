d856ebb8f4eff8bb25a0de58eafe6e58a4f4b4b4
lokki17 <df1985@mail.ru>
2016-11-05 14:22:41 +0300

    HW09

---
+++ src/main/java/ru/javawebinar/topjava/to/MealTo.java
@@ -0,0 +1,62 @@
+package ru.javawebinar.topjava.to;
+
+import java.time.LocalDateTime;
+
+public class MealTo {
+    private Integer id;
+    private LocalDateTime dateTime;
+    private String description;
+    private Integer calories;
+
+    public MealTo() {
+    }
+
+    public MealTo(Integer id, LocalDateTime dateTime, String description, Integer calories) {
+        this.id = id;
+        this.dateTime = dateTime;
+        this.description = description;
+        this.calories = calories;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getCalories() {
+        return calories;
+    }
+
+    public void setCalories(Integer calories) {
+        this.calories = calories;
+    }
+
+    @Override
+    public String toString() {
+        return "MealTo{" +
+                "id=" + id +
+                ", dateTime=" + dateTime +
+                ", description='" + description + '\'' +
+                ", calories=" + calories +
+                '}';
+    }
+}
