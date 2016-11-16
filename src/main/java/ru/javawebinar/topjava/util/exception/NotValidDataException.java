583492beb5d1d255b8a21c33413ac2981168c25c
Lokki <df1985@mail.ru>
2016-11-16 10:30:29 +0300

    HW10

--- src/main/java/ru/javawebinar/topjava/util/exception/NotValidDataException.java
+++ src/main/java/ru/javawebinar/topjava/util/exception/NotValidDataException.java
@@ -1,8 +1,5 @@
 package ru.javawebinar.topjava.util.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
 public class NotValidDataException extends RuntimeException{
     public NotValidDataException(String message) {
         super(message);
